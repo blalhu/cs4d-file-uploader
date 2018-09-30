0e190276f24478f4148c9105b8c7ddd8cd44cc20
Kornél Balatoni <blal.hu@gmail.com>
2018-09-30 12:49:45 +0200

    Upload types

--- UploadType/Base64.ts
+++ UploadType/Base64.ts
@@ -1,31 +1,84 @@
 namespace CS4D {
     export namespace UploadType {
+
+        import UploaderPerformanceType =  CS4D.Options.PerformanceType;
+
         export class Base64 {
 
             private base64;
 
             private plainText;
 
-            constructor ( base64OrPlainText :string ) {
-                try {
-                    this.plainText = atob( base64OrPlainText );
-                    this.base64    = base64OrPlainText;
-                } catch (e) {
-                    this.base64    = btoa( base64OrPlainText );
-                    this.plainText = base64OrPlainText;
+            private performanceStrategy :UploaderPerformanceType;
+
+            constructor ( value :Input, performanceStrategy :UploaderPerformanceType ) {
+                this.performanceStrategy = performanceStrategy;
+                if (value instanceof Base64Input) {
+                    this.base64 = value.getInputValue();
+                    if (performanceStrategy == UploaderPerformanceType.PRELOAD) {
+                        this.plainText = atob( this.base64 );
+                    }
+                    return;
+                }
+                this.plainText = value.getInputValue();
+                if (performanceStrategy == UploaderPerformanceType.PRELOAD) {
+                    this.base64 = btoa( this.plainText );
                 }
             }
 
-            public getBase64()
+            public getBase64():string
             {
+                if (typeof this.base64 != 'undefined') {
+                    return this.base64;
+                }
+                if (this.performanceStrategy == UploaderPerformanceType.ALWAYSLOAD) {
+                    return btoa( this.plainText );
+                }
+                this.base64 = btoa( this.plainText );
                 return this.base64;
             }
 
-            public getPlainText()
+            public getPlainText():string
             {
+                if (typeof this.plainText != 'undefined') {
+                    return this.plainText;
+                }
+                if (this.performanceStrategy == UploaderPerformanceType.ALWAYSLOAD) {
+                    return atob( this.base64 );
+                }
+                this.plainText = atob( this.base64 );
                 return this.plainText;
             }
 
+        }
+
+        interface Input {
+            getInputValue():string;
+        }
+
+        export class Base64Input implements Input{
+            private input :string;
+
+            constructor ( base64 :string ) {
+                this.input = base64;
+            }
+
+            public getInputValue():string {
+                return this.input;
+            }
+        }
+
+
+        export class PlainTextInput implements Input{
+            private input :string;
+
+            constructor ( text :string ) {
+                this.input = text;
+            }
+
+            public getInputValue():string {
+                return this.input;
+            }
         }
     }
 }
--- UploadType/DataUrl.ts
+++ UploadType/DataUrl.ts
@@ -1,11 +1,98 @@
 namespace CS4D {
     export namespace UploadType {
-        export class DataUrl {
+        export class DataUri {
 
-            constructor( dataUrl :string );
-            constructor( data :string, mimeType :string, base64 :boolean );
-            constructor( content :string, mimeType ?:string, base64 ?:boolean ) {
+            constructor( data :DataUriInput ){
+                this.data = data;
             }
+
+            private data :DataUriInput;
+
+            public static PlainTextInput(data :string, mediaType :string = null, shouldBeBase64 :boolean = false) {
+                var input = new DataUriInput();
+                input.plainData = data;
+                input.mediaType  = mediaType;
+                input.isBase64  = shouldBeBase64;
+                return input;
+            }
+
+            public static Base64Input(data :string, mediaType :string = null, shouldBeBase64 :boolean = true) {
+                var input = new DataUriInput();
+                input.base64Data = data;
+                input.mediaType   = mediaType;
+                input.isBase64   = shouldBeBase64;
+                return input;
+            }
+
+            public static DataUriInput(dataUri :string) {
+                var input = new DataUriInput();
+                input.completeData = dataUri;
+                return input;
+            }
+
+            public getPlainData() {
+                if (typeof this.data.plainData == 'undefined') {
+                    if (typeof this.data.base64Data == 'undefined') {
+                        this.data = this.getDataUriParts(this.data.completeData);
+                    }
+                    this.data.plainData = atob( this.data.base64Data );
+                }
+                return this.data.plainData;
+            }
+
+            public getBase64Data() {
+                if (typeof this.data.base64Data == 'undefined') {
+                    if (typeof this.data.plainData == 'undefined') {
+                        this.data = this.getDataUriParts(this.data.completeData);
+                    }
+                    this.data.base64Data;
+                }
+                return this.data.base64Data;
+            }
+
+            public isBase64() {
+                if (typeof this.data.isBase64 == 'undefined') {
+                    this.data = this.getDataUriParts( this.data.completeData );
+                }
+                return this.data.isBase64;
+            }
+
+            public getMediaType() {
+                if (typeof this.data.mediaType == 'undefined') {
+                    this.data = this.getDataUriParts( this.data.completeData );
+                }
+                return this.data.mediaType;
+            }
+
+            private getDataUriParts( dataUri :string ) {
+                var dataUriRegExp = new RegExp('^data\:(.+(?<!\;base64)|)(\;base64|)\,(.*)$'); //TODO: workaround for look behind
+                var processedParts = dataUriRegExp.exec(dataUri);
+                var returnParts = new DataUriInput();
+                returnParts.completeData = dataUri;
+                returnParts.mediaType = processedParts[1];
+                returnParts.isBase64  = false;
+                if ( processedParts[2] == ';base64' ) {
+                    returnParts.isBase64 = true;
+                }
+                if (returnParts.isBase64) {
+                    returnParts.base64Data = processedParts[3];
+                    returnParts.plainData  = atob( processedParts[3] );
+                } else {
+                    returnParts.base64Data = btoa( processedParts[3] );
+                    returnParts.plainData  = processedParts[3];
+                }
+                return returnParts;
+            }
+
+
+        }
+
+        class DataUriInput {
+            completeData;
+            mediaType;
+            isBase64;
+            plainData;
+            base64Data;
         }
 
     }
